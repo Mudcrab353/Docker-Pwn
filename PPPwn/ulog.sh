37bb424cea2d9987e742171bc32e83510d0182bd
Mudcrab353 <lazaro_353@hotmail.com>
2024-08-25 22:48:51 +1000

    Remove Sudo ulog.sh

--- PPPwn/ulog.sh
+++ PPPwn/ulog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f /boot/firmware/PPPwn/upd.log ]; then
-   sudo rm -f /boot/firmware/PPPwn/upd.log
+    rm -f /boot/firmware/PPPwn/upd.log
 fi
 while read -r stdo ; 
 do 
-  echo -e $stdo | sudo tee /dev/tty1 | sudo tee /dev/pts/* | sudo tee -a /boot/firmware/PPPwn/upd.log
-done < <(sudo bash /boot/firmware/PPPwn/update.sh)
+  echo -e $stdo |  tee /dev/tty1 |  tee /dev/pts/* |  tee -a /boot/firmware/PPPwn/upd.log
+done < <( bash /boot/firmware/PPPwn/update.sh)
