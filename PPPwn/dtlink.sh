e5dd8e1175fff498a89ec133adb126f9543fb3ec
Mudcrab353 <lazaro_353@hotmail.com>
2024-08-25 22:41:48 +1000

    Remove Sudo dtlink.sh

--- PPPwn/dtlink.sh
+++ PPPwn/dtlink.sh
@@ -8,24 +8,24 @@ if [ -z $DTLINK ]; then DTLINK=false; fi
 
 
 if [ $DTLINK = true ] ; then
-echo -e "\033[32mMonitoring link\033[0m\n" | sudo tee /dev/tty1
+echo -e "\033[32mMonitoring link\033[0m\n" |  tee /dev/tty1
 coproc read -t 60 && wait "$!" || true
 while [[ $(ifconfig $INTERFACE) == *"RUNNING"* ]]
 do
     coproc read -t 10 && wait "$!" || true
 done
-sudo systemctl stop pppoe
-sudo killall pppoe-server
-sudo ip link set $INTERFACE down
-sudo iptables -P INPUT ACCEPT
-sudo iptables -P FORWARD ACCEPT
-sudo iptables -P OUTPUT ACCEPT
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -F
-sudo iptables -X
-sudo sysctl net.ipv4.ip_forward=0
-sudo sysctl net.ipv4.conf.all.route_localnet=0
-echo -e "\033[32mRestarting PPPwn\033[0m\n" | sudo tee /dev/tty1
-sudo systemctl restart pipwn
+ systemctl stop pppoe
+ killall pppoe-server
+ ip link set $INTERFACE down
+ iptables -P INPUT ACCEPT
+ iptables -P FORWARD ACCEPT
+ iptables -P OUTPUT ACCEPT
+ iptables -t nat -F
+ iptables -t mangle -F
+ iptables -F
+ iptables -X
+ sysctl net.ipv4.ip_forward=0
+ sysctl net.ipv4.conf.all.route_localnet=0
+echo -e "\033[32mRestarting PPPwn\033[0m\n" |  tee /dev/tty1
+ systemctl restart pipwn
 fi
